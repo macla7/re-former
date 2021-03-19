37fddb9e9d763cb4bd997192c3c3afb432aa6c5c
Mitch <MitchelClark1997@outlook.com>
2021-03-20 09:23:32 +1100

    forms with highlighted errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :username, uniqueness: true, length: { in: 5..20 }, presence: true
-  validates :password, length: { in: 6..50 }, format: { with: /(?=.*?[0-9])(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[#?!@$%^&*-])/ }
+  validates :password, presence: true, length: { in: 6..50 }, format: { with: /(?=.*?[0-9])(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[#?!@$%^&*-])/ }
   validates :email, presence: true, uniqueness: true, format: { with: /@/, message: 'Must be a proper email with @ symbol..'}
 end
